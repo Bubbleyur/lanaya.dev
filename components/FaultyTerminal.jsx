a2d1772ceb681cd95ffde4c0dead3f80338d47df
Bubbleyur <alanwijayaok14@gmail.com>
2026-03-04 10:26:05 +0700

    Fix window undefine

--- components/FaultyTerminal.jsx
+++ components/FaultyTerminal.jsx
@@ -235,7 +235,7 @@ export default function FaultyTerminal({
   tint = '#ffffff',
   mouseReact = true,
   mouseStrength = 0.2,
-  dpr = Math.min(window.devicePixelRatio || 1, 1.5),
+  dpr = typeof window !== 'undefined' ? Math.min(window.devicePixelRatio || 1, 1.5) : 1,
   pageLoadAnimation = true,
   brightness = 1,
   className,
